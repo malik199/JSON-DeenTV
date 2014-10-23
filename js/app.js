0f0a3cc6fc4ebe2249e5bb5a331de028f84a7834
Abdul-Malik Ahmad <malik199@hotmail.com>
2014-10-23 01:13:27 -0400

    added video links and watch page with embed script

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function() {
 	var myApp = angular.module('myApp',['truncate']);
-	myApp.controller('myBusStopCtrl', ['$scope', '$http', function ($scope, $http) {
+	myApp.controller('DeenTv_Controller', ['$scope', '$http', function ($scope, $http) {
 	   
 		$scope.realTimeData;
 		var url = "http://deen.tv/?json=get_category_posts&slug=featured&count=20&status=publish&page=1";
@@ -13,7 +13,17 @@
 			  //alert("saf");
 			  console.log("asdfad");
 			});
-	
+		$scope.playvideo = function(currentItem){
+			if(currentItem.custom_fields.dp_video_code){
+				var str = currentItem.custom_fields.dp_video_code.toString();
+				var str = str.replace('<script type="text/javascript" src="http://content.jwplatform.com/players/', '');
+				var justtheid = str.replace('-xHSncDUr.js"></script>', '');
+				$("#ytplayer").html('<iframe src="http://content.jwplatform.com/players/'+justtheid+'-xHSncDUr.html" width="480" height="270" frameborder="0" scrolling="auto"></iframe>');
+			}else{
+				$("#ytplayer").html('<iframe width="560" height="315" src="//www.youtube.com/embed/'+currentItem.custom_fields.youtube_id+'?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+			}
+			$.mobile.navigate( "#watch" );
+		};
 	}]);
 	
 	//filter HTML
