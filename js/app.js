1c3ffda640963446b4fee712741b39f0889c2452
Abdul-Malik Ahmad <malik199@hotmail.com>
2014-10-28 22:18:36 -0400

    Adding methods for youtube and jwplayer videos and changing the json feed

--- js/app.js
+++ js/app.js
@@ -1,36 +1,72 @@
 (function() {
-	var myApp = angular.module('myApp',['truncate']);
-	myApp.controller('DeenTv_Controller', ['$scope', '$http', function ($scope, $http) {
-	   
-		$scope.realTimeData;
-		var url = "http://deen.tv/?json=get_category_posts&slug=featured&count=20&status=publish&page=1";
-		url += "&callback=JSON_CALLBACK"
-		$http.jsonp(url).success(function(data){
-				$scope.realTimeData = data.posts;
-				console.log(data.found);
-				
-			}).error(function(data) {
-			  //alert("saf");
-			  console.log("asdfad");
-			});
-		$scope.playvideo = function(currentItem){
-			if(currentItem.custom_fields.dp_video_code){
-				var str = currentItem.custom_fields.dp_video_code.toString();
-				var str = str.replace('<script type="text/javascript" src="http://content.jwplatform.com/players/', '');
-				var justtheid = str.replace('-xHSncDUr.js"></script>', '');
-				$("#ytplayer").html('<iframe src="http://content.jwplatform.com/players/'+justtheid+'-xHSncDUr.html" width="480" height="270" frameborder="0" scrolling="auto"></iframe>');
-			}else{
-				$("#ytplayer").html('<iframe width="560" height="315" src="//www.youtube.com/embed/'+currentItem.custom_fields.youtube_id+'?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    var myApp = angular.module('myApp', ['truncate']);
+    myApp.controller('DeenTv_Controller', ['$scope', '$http', function($scope, $http) {
+
+        $scope.realTimeData;
+		var url = "http://deen.tv/?json=get_category_posts&slug=featured&count=50&status=publish&page=1&custom_fields=_tern_wp_youtube_video,dp_video_code";
+        //var url = "http://deen.tv/?json=get_category_posts&slug=featured&count=20&status=publish&page=1";
+        url += "&callback=JSON_CALLBACK"
+        
+		//get jason data
+		$http.jsonp(url).success(function(data) {
+            $scope.realTimeData = data.posts;
+			$scope.numbofpages = data.pages;
+			//alert($scope.numbofpages.toString());
+            //console.log(data.found);
+        }).error(function(data) {
+            //console.log("error");
+        });
+		
+		
+		//play the video
+        $scope.playvideo = function(currentItem) {
+            if (currentItem.dp_video_code) {
+                var str = currentItem.dp_video_code.toString();
+                var str = str.replace('<script type="text/javascript" src="http://content.jwplatform.com/players/', '');
+                var justtheid = str.replace('-xHSncDUr.js"></script>', '');
+                $("#ytplayer").html('<iframe src="http://content.jwplatform.com/players/' + justtheid + '-xHSncDUr.html" width="480" height="270" frameborder="0" scrolling="auto"></iframe>');
+            } else if (currentItem._tern_wp_youtube_video) {
+                $("#ytplayer").html('<iframe width="560" height="315" src="//www.youtube.com/embed/' + currentItem._tern_wp_youtube_video + '?autoplay=1&wmode=opaque&rel=0&showinfo=0&modestbranding=0" frameborder="0" allowfullscreen></iframe>');
+            } else {
+				alert("error" + " " + currentItem);
 			}
-			$.mobile.navigate( "#watch" );
-		};
-	}]);
-	
-	//filter HTML
-	myApp.filter('unsafe', function($sce) {
-		return function(val) {
-			return $sce.trustAsHtml(val);
-		};	
-	});
+            $.mobile.navigate("#watch");
+        };
+		
+		$scope.getcategory = function(cat_Array){
+			var thecat = "";
+    		angular.forEach(cat_Array, function(category) {
+     			if(category.id == 41 || category.id == 4 || category.id == 44 || category.id == 1){
+					//do nothing
+				}else{
+					thecat += category.title;
+				}
+    		});
+			
+			if (thecat == ""){
+				return "Featured";
+			}else{
+				return thecat;
+			}
+		}
+    }]);
+
+    //filter HTML
+    myApp.filter('unsafe', function($sce) {
+        return function(val) {
+            return $sce.trustAsHtml(val);
+        };
+    });
+
+
 
 }());
+$(document).ready(function() {
+    $("#remove").click(function() {
+        $("#ytplayer").empty();
+    });
+    window.onhashchange = function() {
+        //$("#ytplayer").empty();
+    }
+
+});
